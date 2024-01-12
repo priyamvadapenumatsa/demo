2973802a69c9b15d66d5369601dea38d5f20de7a
priyamvadapenumatsa <143866025+priyamvadapenumatsa@users.noreply.github.com>
2024-01-12 11:06:37 +0530

    Update picnvideo.py

--- picnvideo.py
+++ picnvideo.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.header('jeep')
-st.write('jeep')
+st.header('lovely song')
+st.write('yellae lama')
 st.image('https://cdni.autocarindia.com/ExtraImages/20210406061737_Mahindra_Thar_RT_lead.jpg',caption='Jeep')
 st.video('https://youtu.be/tjPIc-S-Mpg?si=yD0PbgxVTaV7NvWC')
