de57d95b9c6d882f15004c24d7e4eba9aa03372b
priyamvadapenumatsa <143866025+priyamvadapenumatsa@users.noreply.github.com>
2024-01-12 11:01:15 +0530

    Update jeep.py

--- jeep.py
+++ jeep.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.header('jeep')
 st.write('jeep')
-st.image('https://cdni.autocarindia.com/ExtraImages/20210406061737_Mahindra_Thar_RT_lead.jpg',caption='jeep')
+st.image('https://cdni.autocarindia.com/ExtraImages/20210406061737_Mahindra_Thar_RT_lead.jpg',caption='Jeep')
+st.video('https://youtu.be/tjPIc-S-Mpg?si=yD0PbgxVTaV7NvWC',caption=lovely song')
