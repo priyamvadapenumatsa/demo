76fc41fe4abd14477701d55720e53d80805489a1
priyamvadapenumatsa <143866025+priyamvadapenumatsa@users.noreply.github.com>
2024-01-12 11:03:10 +0530

    Update and rename jeep.py to picnvideo.py

--- picnvideo.py
+++ picnvideo.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.header('jeep')
 st.write('jeep')
 st.image('https://cdni.autocarindia.com/ExtraImages/20210406061737_Mahindra_Thar_RT_lead.jpg',caption='Jeep')
-st.video('https://youtu.be/tjPIc-S-Mpg?si=yD0PbgxVTaV7NvWC',caption='lovely song')
+st.video('https://youtu.be/tjPIc-S-Mpg?si=yD0PbgxVTaV7NvWC')
